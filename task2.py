5c6b51b57103fe1121a6b9a1e28153b7c630e3b3
Ishani-Arora11 <aroraishani005@gmail.com>
2026-01-08 23:42:49 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+"""
+Take first and last name as input
+Display a personalized greeting
+"""
+
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+
+name = f_name + " " + l_name
+
+print("Hello,",name,"! Welcome to the Python program.")
